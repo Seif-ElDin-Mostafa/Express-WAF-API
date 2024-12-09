35a0e0760688efc238068b5d076969bfefb621ca
Seif ElDein Mostafa <seifeldin-mostafa@outlook.com>
2024-12-09 17:32:22 +0200

    ...

--- src/app.mjs
+++ src/app.mjs
@@ -1,7 +1,7 @@
 import express from "express"
 import startServer from "./helpers/startServer.mjs"
 
-const app = express();
+export const app = express();
 startServer();
 
 app.use(express.json());
